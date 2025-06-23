cd7f2b8b472cd168c7353fcc74827c3a51ed4b77
RameshAnnabhimoju <ramesh.annabhimoju@gmail.com>
2025-06-23 10:43:49 +0530

    added c

--- api/index.js
+++ api/index.js
@@ -29,7 +29,7 @@ app.post("/send-email", async (req, res) => {
     const transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST,
       port: process.env.SMTP_PORT,
-      secure: process.env.SMTP_SECURE === "true", // true for 465, false for other ports
+      secure: process.env.SMTP_SECURE, // true for 465, false for other ports
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
